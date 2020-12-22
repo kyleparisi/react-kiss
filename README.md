11d5579dfebb56c8451938dda9b062a1a0f6f602
Kyle Parisi <kyle@saytechnologies.com>
2020-12-22 17:11:41 -0500

    webpack

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ```bash
 npm install
-npm run browserify
+npm run build
 open index.html
 ```
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: "./index.jsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-react"] }
+      }
+    ]
+  }
+}
