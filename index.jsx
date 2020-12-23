470a7c0f470c8a3118162800def37850189df5e2
Kyle Parisi <kyle@saytechnologies.com>
2020-12-22 20:53:57 -0500

    reactive

--- index.jsx
+++ index.jsx
@@ -1,6 +1,39 @@
-const App = () => <h1>Hello world!</h1>
+const handler = {
+  get(target, key) {
+    if (typeof target[key] === 'object' && target[key] !== null) {
+      return new Proxy(target[key], handler)
+    } else {
+      return target[key];
+    }
+  },
+  set: function(obj, prop, value) {
+    obj[prop] = value;
+    console.log("render")
+    render();
+  }
+};
+const state = new Proxy({
+  name: "Kyle Parisi",
+  nested: {
+    value: true
+  }
+}, handler);
+window.state = state;
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+
+const App = () => {
+  return (<div>
+    <h1>Hello {state.name}!</h1>
+    {state.nested.value && <div>nested value</div>}
+  </div>)
+}
+window.App = App
+
+function render() {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  );
+}
+render();
+window.render = render;
